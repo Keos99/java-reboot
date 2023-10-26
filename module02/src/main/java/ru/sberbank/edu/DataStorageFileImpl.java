c1b324d72154d83c89d77d0495be9de025fef709
keos99 <keos99@yandex.ru>
2023-10-27 00:25:42 +0300

    - Классы для сохранения данных

---
+++ module02/src/main/java/ru/sberbank/edu/DataStorageFileImpl.java
@@ -0,0 +1,29 @@
+package ru.sberbank.edu;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class DataStorageFileImpl implements DataStorage {
+
+    private BufferedWriter writer;
+    private File file;
+
+    public DataStorageFileImpl(String filePath) {
+        file = new File(filePath);
+    }
+
+    @Override
+    public void save(int lineCount, int spaceCount, String line) {
+        try {
+            writer = new BufferedWriter(new FileWriter(file));
+            writer.write("Количество строк: " + lineCount + " Количество пробелов: " + spaceCount +
+                    " Самая длинная строка:\n" + line);
+            writer.newLine();
+            writer.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
